2a3e4f85713ab513e646efda2c1702986d6a5fa4
Denis <klop1280@gmail.com>
2019-08-31 14:49:23 +0300

    Created Food and BonusFood classes

---
+++ src/classes/BonusFood.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lewan on 31.08.19.
+//
+
+#include "BonusFood.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,5 @@
 int main()
 {
     std::list<int> tmp = {1, 2, 3, 4, 5};
-	SnakeHead snakeHead(new std::string("Head avatar path"));
-	Snake snake(snakeHead);
 
-	std::cout << "<tail(x:" << snake.getBody().begin()->getPossition().x << ", y:" <<
-			snake.getBody().begin()->getPossition().y << ")";
-    for (auto i = snake.getBody().begin(); i != snake.getBody().end(); i = std::next(i, 1))
-    {
-    	auto next_part = std::next(i, 1);
-        if (next_part != snake.getBody().end())
-            std::cout << "(x:" << next_part->getPossition().x << ", y:" << next_part->getPossition().y << ")";
-    }
-    std::cout << "(x:" << snake.getHead().getPossition().x << ", y:"
-    		<< snake.getHead().getPossition().y << ")head}" << std::endl;
 }
